75393dbfc455d5e01efac49373f62bde7df5a189
Leandro G. da Cruz <LeandroCruz717@outlook.com>
2025-05-26 22:53:16 -0300

    tests: add CqrsCommandQueryResolver tests

--- Cqrs.Core/RegisterResolver/Resolver.cs
+++ Cqrs.Core/RegisterResolver/Resolver.cs
@@ -11,12 +11,18 @@ public sealed class CqrsCommandQueryResolver
     internal readonly Dictionary<Type, Type> QueryHandlers = new();
 
     public bool TryGetCommandHandler(
-        Type command, 
-        [NotNullWhen(true)] out Type? commandHandler) =>
-        CommandHandlers.TryGetValue(command, out commandHandler);
+        Type command,
+        [NotNullWhen(true)] out Type? commandHandler)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+        return CommandHandlers.TryGetValue(command, out commandHandler);
+    }
 
     public bool TryGetQueryHandler(
-        Type query, 
-        [NotNullWhen(true)] out Type? queryHandler) => 
-        QueryHandlers.TryGetValue(query, out queryHandler);
+        Type query,
+        [NotNullWhen(true)] out Type? queryHandler)
+    {
+        ArgumentNullException.ThrowIfNull(query);
+        return QueryHandlers.TryGetValue(query, out queryHandler);
+    }
 }
---
+++ Cqrs.Tests/RegisterResolver/Resolver.cs
@@ -0,0 +1,30 @@
+﻿namespace Cqrs.Test.RegisterResolver;
+
+public class ResolverTests
+{
+    private CqrsCommandQueryResolver _resolver;
+
+    [SetUp]
+    public void Setup()
+    {
+        _resolver = new CqrsCommandQueryResolver();
+    }
+
+    [Test]
+    public void TryGetCommandHandler_WithNull_ShouldThrow()
+    {
+        var ex = Assert.Throws<ArgumentNullException>(() =>
+            _resolver.TryGetCommandHandler(null!, out _));
+
+        Assert.That(ex.ParamName, Is.EqualTo("command"));
+    }
+
+    [Test]
+    public void TryGetQueryHandler_WithNull_ShouldThrow()
+    {
+        var ex = Assert.Throws<ArgumentNullException>(() =>
+            _resolver.TryGetQueryHandler(null!, out _));
+
+        Assert.That(ex.ParamName, Is.EqualTo("query"));
+    }
+}
